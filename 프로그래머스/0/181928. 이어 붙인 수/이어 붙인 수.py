f92fb9f7675746c6ce2114a7933474a65e804aad
Yeon dong <rchijy@naver.com>
2024-02-01 00:45:17 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    even = ''
+    odd = ''
+    for i in num_list:
+        if i % 2:
+            even += str(i)
+        else:
+            odd += str(i)
+    return int(even) + int(odd)
