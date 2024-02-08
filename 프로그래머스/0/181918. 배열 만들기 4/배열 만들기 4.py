fccd50fb8f1e7073d10a993295ee56ec463284a9
Yeon dong <rchijy@naver.com>
2024-02-09 01:07:06 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.17 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    i=0
+    stk = []
+    while i < len(arr):
+        if stk == []:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        else:
+            stk.pop()
+    return stk
