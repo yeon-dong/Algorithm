4042145b89f32332b8d6bee9b5a44e2c4d40329b
Yeon dong <rchijy@naver.com>
2024-02-13 18:03:13 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,8 +1,8 @@
 def solution(my_string):
     answer = [0 for _ in range(52)]
-    for i in range(len(my_string)):
-        if my_string[i].isupper():
-            answer[ord(my_string[i])-65] += 1
+    for i in my_string:
+        if i.isupper():
+            answer[ord(i)-65] += 1
         else:
-            answer[ord(my_string[i])-71] += 1
+            answer[ord(i)-71] += 1
     return answer
