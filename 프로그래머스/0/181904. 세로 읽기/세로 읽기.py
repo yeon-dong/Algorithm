296e9d191bac100d390fe98ffcbda197bd16b08a
Yeon dong <rchijy@naver.com>
2024-02-15 18:36:32 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,7 +1,2 @@
 def solution(my_string, m, c):
-    answer = ''
-    a = 0
-    while a < len(my_string):
-        answer += my_string[c-1+a]
-        a += m
-    return answer
+    return my_string[c-1::m]
