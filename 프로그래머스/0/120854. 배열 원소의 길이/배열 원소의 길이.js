22f13f4c0df555897ab40edea54ee91685e4e25d
Yeon dong <rchijy@naver.com>
2024-04-06 11:05:32 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,3 @@
 function solution(strlist) {
-    var answer = [];
-    for(var i = 0; i<strlist.length; i++){
-        answer.push(strlist[i].length)
-    }
-    return answer;
+    return strlist.map((a) => a.length);
 }
