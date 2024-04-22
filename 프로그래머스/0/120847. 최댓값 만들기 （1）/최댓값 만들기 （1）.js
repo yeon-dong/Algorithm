00c081b5fe9253f2ec0b2b0848f611dc00084a60
Yeon dong <rchijy@naver.com>
2024-04-22 11:32:10 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,6 +1,4 @@
 function solution(numbers) {
-    var a = Math.max(...numbers);
-    numbers.splice(numbers.indexOf(a),1)
-    var b = Math.max(...numbers);
-    return a*b;
+    numbers.sort((a,b)=>b-a);
+    return numbers[0]*numbers[1];
 }
