6ab92e3594d1fc090a944a59c429d235e9a91de4
Yeon dong <rchijy@naver.com>
2024-04-23 11:34:05 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,8 +1,3 @@
 function solution(my_string) {
-    let answer = my_string.replaceAll('a', '');
-    answer = answer.replaceAll('e', '');
-    answer = answer.replaceAll('i', '');
-    answer = answer.replaceAll('o', '');
-    answer = answer.replaceAll('u', '');
-    return answer;
+    return my_string.replace(/[aeiou]/g, '');
 }
