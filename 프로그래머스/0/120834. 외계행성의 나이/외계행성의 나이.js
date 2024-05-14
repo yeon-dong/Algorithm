b07917098b2455c01bde380748ba80ac08f6c052
Yeon dong <rchijy@naver.com>
2024-05-14 22:12:34 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,8 +1,8 @@
 function solution(age) {
-    age = age.toString()
-    var answer = '';
-    for(let i = 0; i < age.length; i++){
-        answer += String.fromCharCode(parseInt(age[i])+97);
-    }
-    return answer;
+    return age
+    .toString()
+    .split("")
+    .map((v) => "abcdefghij"[v])
+    .join("");
+    // 52 -> "52" -> ["5","2"]. -> ["abcdefghij"[5], "abcdefghij"[2] ] -> "fc"
 }
