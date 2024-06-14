82f335383410d91d68626aa47fde1a4b34c2cab8
Yeon dong <rchijy@naver.com>
2024-06-14 10:07:43 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,14 +1,5 @@
 function solution(s) {
-    s=s.split(' ')
-    min = Number(s[0]);
-    max = Number(s[0]);
-    for(let i = 1; i < s.length; i++){
-        if(min > Number(s[i])){
-            min = Number(s[i]);
-        }
-        if(max < Number(s[i])){
-            max = Number(s[i]);
-        }
-    }
-    return String(min) + " " + String(max);
+    const arr = s.split(' ');
+
+    return Math.min(...arr)+' '+Math.max(...arr);
 }
