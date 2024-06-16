ffcadb1254f3fadf292350dc347f6e5d544ed207
Yeon dong <rchijy@naver.com>
2024-06-16 23:26:30 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,12 +1,3 @@
 function solution(a, b) {
-    var answer = 0;
-    if(b<a){
-        let space = b;
-        b = a;
-        a = space;
-    }
-    for(; a<=b; a++){
-        answer += a;
-    }
-    return answer;
+    return (a+b) * (Math.abs(a-b)+1) / 2;
 }
