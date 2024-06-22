056fd40f4347c56e2035102fb46c647bd77d0848
Yeon dong <rchijy@naver.com>
2024-06-22 15:18:41 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,6 +1,3 @@
 function solution(numbers) {
-    let sum = 0;
-    [0,1,2,3,4,5,6,7,8,9].filter((n) => !numbers.includes(n))
-        .forEach(num => {sum += num;});
-    return sum;
+    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
 }
