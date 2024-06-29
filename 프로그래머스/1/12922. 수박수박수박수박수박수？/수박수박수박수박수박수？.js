8d4d473a86711bd88afda4af721c3704b6a755ff
Yeon dong <rchijy@naver.com>
2024-06-30 06:40:54 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,1 @@
-function solution(n) {
-    var answer = '';
-    for(let i = 0; i < n; i++){
-        i % 2 ? answer += '박' : answer += '수';
-    }
-    return answer;
-}
+const solution = n => "수박".repeat(n/2+1).slice(0,n);
