ce6835c656ce692a5cbbfcb6c2e133b4614c7ae3
Yeon dong <rchijy@naver.com>
2024-07-02 11:33:45 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,3 @@
 function solution(a, b) {
-    var answer = 0;
-    for(let i = 0; i < a.length; i++){
-        answer += a[i]*b[i]
-    }
-    return answer;
+    return a.reduce((acc, _, i) => acc += a[i] * b[i], 0);
 }
