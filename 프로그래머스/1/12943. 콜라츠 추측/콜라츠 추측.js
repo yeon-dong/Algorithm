c8d00a6ec388b535ee27824abf9504f02340c156
Yeon dong <rchijy@naver.com>
2024-07-03 19:08:59 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,10 +1,8 @@
 function solution(num) {
-    var count = 0;
-    while(num != 1){
-        if(num % 2){ num = num*3 + 1 }
-        else{ num = num/2 }
-        if(count > 500){return -1}
-        count++;
-    }
-    return count;
+    var answer = 0;
+    while(num !=1 && answer !=500){
+        num%2==0 ? num = num/2 : num = num*3 +1;
+    answer++;
+  }
+    return num == 1 ? answer : -1;
 }
