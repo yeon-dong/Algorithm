cbcff73df5f315bd758a754ea37c1add196f051f
Yeon dong <rchijy@naver.com>
2024-07-08 15:41:29 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,7 +1,4 @@
 function solution(price, money, count) {
-    var answer = 0;
-    for(let i = 1; i <= count; i++){
-        answer += price * i;
-    }
-    return answer - money > 0 ? answer - money : 0;
+    const tmp = price * count * (count + 1) / 2 - money;
+    return tmp > 0 ? tmp : 0;
 }
