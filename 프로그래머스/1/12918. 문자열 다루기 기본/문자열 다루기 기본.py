6af90b04602be776d52697ea4e894fce42430ed7
Yeon dong <rchijy@naver.com>
2024-07-11 14:02:23 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,7 +1,2 @@
 def solution(s):
-    if len(s) != 6 and len(s) != 4:
-        return False
-    for i in s:
-        if i not in ["0","1","2","3","4","5","6","7","8","9"]:
-            return False
-    return True
+    return s.isdigit() and len(s) in [4,6]
