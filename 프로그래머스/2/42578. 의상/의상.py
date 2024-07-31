1e24541727ee2935f306830c897b954d9f45e70e
Yeon dong <rchijy@naver.com>
2024-07-31 09:15:00 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,8 @@
+def solution(clothes):
+    hash_map = {}
+    for name, type in clothes:
+        hash_map[type] = hash_map.get(type,0) + 1
+    answer = 1
+    for type in hash_map:
+        answer *= hash_map[type] + 1
+    return answer - 1
