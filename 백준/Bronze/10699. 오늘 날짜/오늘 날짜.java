82b4fefb312d2c6ba379939fa5661796b43738ea
Yeon dong <rchijy@naver.com>
2024-09-25 15:40:03 +0900

    [Bronze V] Title: 오늘 날짜, Time: 160 ms, Memory: 17148 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now(); // 오늘 날짜 가져오기
+        System.out.println(today); // 출력: 오늘 날짜: YYYY-MM-DD 형식
+    }
+}
