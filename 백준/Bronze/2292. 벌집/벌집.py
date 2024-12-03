5728ef5bf4f8b1b7488efd91bf51474ccab32a86
Yeon dong <rchijy@naver.com>
2024-12-03 20:00:20 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,16 +1,10 @@
 import sys
 
 N = int(input())
-i=2
+i = 1
 hexagon = 1
-if N == 1: # 1이면 한칸 지나니까 1을 출력
-    print(1)
 # i는 0부터 N과 같거나 작을때 까지
-while i <= N:
-    # N이 몇번째 육각형 안에 포함되는지.
-    if(N < i + hexagon * 6):
-        print(hexagon+1)
-        break
-    else: #육각형에 포함 안되면 육각형 하나 추가, i도 다음 육각형으로 보냄
-        i += hexagon * 6
-        hexagon += 1
+while i < N:
+    i += 6 * hexagon # 다음 박스까지 i를 키워 놓음.
+    hexagon += 1 # 박스를 키워놨으니 1칸 더 가야 하는 숫자
+print(hexagon)
