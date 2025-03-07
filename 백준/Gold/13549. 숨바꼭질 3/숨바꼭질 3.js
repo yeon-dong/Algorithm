92874a20742345a8cf7bf7db78e19bce101ba56f
Yeon dong <rchijy@naver.com>
2025-03-07 22:23:43 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 188 ms, Memory: 17500 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -5,7 +5,59 @@ const [N, K] = require("fs")
   .split(" ")
   .map(Number);
 
-const queue = [];
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue{
+  constructor(){
+    this.front = null;
+    this.rear = null;
+    this.length = 0;
+  }
+
+  push(value){ //요소 추가
+    const newNode = new Node(value);
+    if(!this.front){ // 큐가 비어있다면
+      this.front = this.rear = newNode;
+    }else{
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
+    this.length++;
+  }
+
+  shift(){
+    if (!this.front) return null;  // 큐가 비어있다면 null 반환
+    const dequeuedValue = this.front.value;
+    this.front = this.front.next;
+    if (!this.front) this.rear = null; // 큐가 비었다면 rear도 null 처리
+    this.length--;
+    return dequeuedValue;
+  }
+
+  unshift(value){
+    const newNode = new Node(value);
+    if(!this.front){ // 큐가 비어있다면
+      this.front = this.rear = newNode;
+    }else{
+      const nextNode = this.front;
+      this.front = newNode;
+      this.front.next = nextNode;
+    }
+    this.length++;
+  }
+
+  length(){
+    return this.length;
+  }
+}
+
+const queue = new Queue;
 queue.push([N,0]);
 const visited = new Array(100002).fill(0);
 while (queue.length) {
@@ -22,72 +74,3 @@ while (queue.length) {
     }
   }
 }
-
-
-// class Node{
-//   constructor(value){
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
-
-// class Queue{
-//   constructor(){
-//     this.front = null;
-//     this.rear = null;
-//     this.length = 0;
-//   }
-
-//   push(value){ //요소 추가
-//     const newNode = new Node(value);
-//     if(!this.front){ // 큐가 비어있다면
-//       this.front = this.rear = newNode;
-//     }else{
-//       this.rear.next = newNode;
-//       this.rear = newNode;
-//     }
-//     this.length++;
-//   }
-
-//   shift(){
-//     if (!this.front) return null;  // 큐가 비어있다면 null 반환
-//     const dequeuedValue = this.front.value;
-//     this.front = this.front.next;
-//     if (!this.front) this.rear = null; // 큐가 비었다면 rear도 null 처리
-//     this.length--;
-//     return dequeuedValue;
-//   }
-
-//   frontPush(value){
-//     const newNode = new Node(value);
-//     if(!this.front){ // 큐가 비어있다면
-//       this.front = this.rear = newNode;
-//     }else{
-//       const nextNode = this.front;
-//       this.front = newNode;
-//       this.front.next = nextNode;
-//     }
-//     this.length++;
-//   }
-// }
-
-// const queue = new Queue;
-// const visited = new Array(100002).fill(0);
-// queue.push(N);
-// while(true){
-//   const now = queue.shift();
-//   if(now == K){
-//     console.log(visited[now]);
-//     break;
-//   }
-//   for(let i of [now-1,now+1]){
-//     if(i >= 0 && i < 100000 && !visited[i]){
-//       queue.push(i);
-//       visited[i] = visited[now] + 1;
-//     }
-//   }
-//   if(now * 2 > 0 && now * 2 < 100000 && !visited[now * 2]){
-//     queue.frontPush(now * 2);
-//     visited[now * 2] = visited[now];
-//   }
-// }
